cc756ae5eaa8092b17a3d4d642f26849715177ab
Mirko Adari <mirko.eesti@gmail.com>
2012-10-08 10:25:39 +0300

    Fixed file download.

--- concurrency3/src/main/java/ee/ut/jf/concurrency/Task.java
+++ concurrency3/src/main/java/ee/ut/jf/concurrency/Task.java
@@ -3,8 +3,10 @@ package ee.ut.jf.concurrency;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
 import java.util.concurrent.Callable;
 
 public class Task implements Callable<String> {
@@ -18,10 +20,21 @@ public class Task implements Callable<String> {
     String fileName = file.getPath().substring(file.getPath().lastIndexOf("/") + 1);
 
     ReadableByteChannel rbc = Channels.newChannel(file.openStream());
+    ByteBuffer buffer = ByteBuffer.allocateDirect(16 * 1024);
+
     FileOutputStream fos = null;
     try {
       fos = new FileOutputStream(fileName);
-      fos.getChannel().transferFrom(rbc, 0, 1 << 24);
+      WritableByteChannel wbc = Channels.newChannel(fos);
+
+      while (rbc.read(buffer) != -1) {
+        buffer.flip();
+        wbc.write(buffer);
+        buffer.compact();
+      }
+      buffer.flip();
+      while (buffer.hasRemaining())
+        wbc.write(buffer);
     }
     finally {
       if (fos != null)
